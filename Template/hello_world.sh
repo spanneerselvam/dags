c96a52864c2bf0aff3fc623a81048029d6033aeb
Saigopini Panneerselvam <spanneerselvam@scu.edu>
2020-04-03 10:56:52 -0700

    Update hello_world.sh

--- Template/hello_world.sh
+++ Template/hello_world.sh
@@ -2,5 +2,5 @@
 #This is a sample bash script that you can edit. 
 
 echo "Hello World!"
-echo "You can overwrite this shell script with your code. Just rename mee and delete lines 2-5. Make sure you save me in the folder you've created."
+echo "You can overwrite this shell script with your code. Just rename me and delete lines 2-5. Make sure you save me in the folder you've created."
 
