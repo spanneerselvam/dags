559cfe4144e6c6f30b1191e9a96dbe99179aff9c
Saigopini Panneerselvam <spanneerselvam@scu.edu>
2020-04-03 10:47:19 -0700

    Create hello_world.sh

---
+++ Template/hello_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is a sample bash script that you can edit. 
+
+echo "Hello World!"
+echo "You can overwrite this shell script with your code. Just rename mee and delete lines 2-5. Make sure you save me in the folder you've created."
+
